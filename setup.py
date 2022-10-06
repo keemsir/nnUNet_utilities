293bb84d31d3a243a2607294c0f5f95fa6e8e6ea
keemsir <74044742+keemsir@users.noreply.github.com>
2022-10-06 10:04:29 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='nn_utils',
+  version='0.0.1',
+  url='https://github.com/keemsir/nnUNet_utilities',
+  author='keemsir',
+  author_email='keemsir@gmail.com',
+  license='keemsir'
+)
